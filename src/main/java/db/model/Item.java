25f7a1e1b5f2bc71f8c97e5e54eada9430138c50
andreishilenko <asj931@yahoo.com>
2022-04-13 21:05:21 +0300

    add some configs for flyaway

--- src/main/java/db/model/Item.java
+++ src/main/java/db/model/Item.java
@@ -1,5 +1,32 @@
 package db.model;
 
-public class Item {
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private long amount;
+
+    @Column(nullable = false)
+    Price price;
+
+
+}
+
+class Price {
+    private int rate;
+    private long value;
 }
